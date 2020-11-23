45ea7c73f5e00c039686337ad55095b3d45b6602
kyhong222 <kyhong222@naver.com>
2020-11-23 14:56:31 +0900

    first

---
+++ src/Components/Router.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
+import Home from "../Routes/Home"
+import TV from "../Routes/TV"
+import Search from "../Routes/Search"
+import Header from "Components/Header"
+// // import Detail from "../Routes/Detail"
+
+export default () => (
+  <Router>
+    <>
+    <Header/>
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/tv' exact component={TV} />
+      <Route path='/search' component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
+    </>
+  </Router>
+)
